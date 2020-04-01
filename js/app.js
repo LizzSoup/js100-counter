2af7ebfc1af87b09dfef1d4c6f8393db3c9f0df1
lizz <elizabeth.ann.soup@gmail.com>
2020-04-01 16:06:39 -0400

    Challenge solved

--- js/app.js
+++ js/app.js
@@ -1,28 +1,18 @@
-(function(){
-  const buttons = document.querySelectorAll('.counterBtn')
-  let count= 0
+const lower = document.getElementById('lower');
+const higher = document.getElementById('higher');
+const counter = document.getElementById('counter');
 
+let count = 0;
 
-  //Add event listeners and functionailty to each button  
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(){
-      if (button.classList.contains('prevBtn')){
-        count--
-      } else if (button.classList.contains('nextBtn')){
-        count++
-      }
+higher.addEventListener('click', add);
+lower.addEventListener('click', subtract);
 
-      //Select the counter text
-      const counter = document.querySelector('#counter')
-      counter.textContent = count
+function add() {
+  count++;
+  counter.innerHTML = count;
+}
 
-      if (count < 0 ){
-        counter.style.color = 'red'
-      } else if (count > 0){
-        counter.style.color = 'green'
-      } else {
-        counter.style.color = '#333333'
-      }
-    })
-  })
-})()
+function subtract() {
+  count -= 1;
+  counter.innerHTML = count;
+}
